13f53d46dedf4f2b8d23e450725c40dfcc4a1487
Denys Basarab <denya2007basarab@gmail.com>
2025-12-17 16:16:50 +0200

    delete magic number and improve logic

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,17 +1,19 @@
 package core.basesyntax;
 
 public class Main {
+    public static final int ARRAY_SIZE = 6;
+    public static final int HALF_OF_ARRAY_SIZE = ARRAY_SIZE / 2;
+
     public static void main(String[] args) {
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[ARRAY_SIZE];
         FigureSupplier figureSupplier = new FigureSupplier();
-        for (int i = 0; i < 3; i++) {
-            figures[i] = figureSupplier.getRandomFigure();
-        }
-        for (int i = 3; i < 6; i++) {
-            figures[i] = figureSupplier.getDefaultFigure();
-        }
-        for (Figure figure : figures) {
-            figure.draw();
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            if (i < HALF_OF_ARRAY_SIZE) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+            figures[i].draw();
         }
     }
 }
